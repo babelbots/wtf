f20e69ac4d459b31242c1d64e876c0044b8a85bd
Cris <victor@babelgames.es>
2026-07-08 23:46:50 +0200

    fix: import useEffect in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Screen } from './types';
 import { LoginScreen } from './screens/LoginScreen';
 import { WodScreen } from './screens/WodScreen';
